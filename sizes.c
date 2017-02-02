201d6ee1f254763ea8a98970c1f31e733e739d15
feakos <fekete@feketeakos.com>
2017-02-02 22:20:28 +0100

    hozzáadtam a long long-ot és a long double-t

--- sizes.c
+++ sizes.c
@@ -6,9 +6,11 @@ int main(){
 	printf("int: %lu\n", sizeof(int));
 	printf("short: %lu\n", sizeof(short));
 	printf("long: %lu\n", sizeof(long));
+	printf("long long: %lu\n", sizeof(long long));
 	printf("char: %lu\n", sizeof(char));
 	printf("float: %lu\n", sizeof(float));
 	printf("double: %lu\n", sizeof(double));
+	printf("long double: %lu\n", sizeof(long double));
 	printf("void*: %lu\n", sizeof(void*));
 
 	return 0;
