ae033bcb48bb4c83d0051308774198f782459e29
feakos <fekete@feketeakos.com>
2017-02-03 11:59:43 +0100

    Long intet alakít át bináris számmá, hibát is kezel. strtol() fgv-t használtam erre

--- bit.c
+++ bit.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
 
-void decToBin(int number);
-void decToBin2(int number);
+void decToBin(long int number);
+
+#define MAX_BITS 64
+#define BUFFER_SIZE 128
+#define BASE 10
 
 int main(){
-	int number;
+	long number;
+	char *end, buffer[BUFFER_SIZE];
+	errno = 0;
 
 	puts("Adj meg egy decimális számot: ");
-	scanf("%d", &number);
-	decToBin(number);
-	//decToBin2(number);
+	fgets(buffer, BUFFER_SIZE, stdin);
+	
+	number = strtol(buffer, &end, BASE);
+		
+	if ((errno == ERANGE && (number == LONG_MAX || number == LONG_MIN)) 
+		|| (errno != 0 && number == 0)){
+    	fprintf(stderr, "Ez sajnos több, mint 64 bit!\n");
+    	exit(1);
+    }
+    else{
+		decToBin(number);
+	}
 
 	return 0;
 }
 
-void decToBin(int number){
-	char maradek[20];
+void decToBin(long int number){
+	char maradek[MAX_BITS];
 	int i = 0;
 
 	if(number < 0){	
-		printf("Sajnos negativ szamot adtal meg. Holnap visszaterunk ra!\n");
-		printf("De addig is negalom neked %i\n", -number);
+		fprintf(stderr, "Sajnos negativ szamot adtal meg. Holnap visszaterunk ra!\n");
+		fprintf(stderr, "De addig is negalom neked %li\n", -number);
 /*		number = -number;
 		while (number != 0){
 			if((number & 1) == 0)
@@ -45,7 +61,7 @@ void decToBin(int number){
 			i++;
 		}
 		maradek[i] = 0;
-	
+		
 		char temp[i + 1];
 		int j = 0;
 		while(i >= 0){
@@ -55,39 +71,9 @@ void decToBin(int number){
 		}
 
 		for(i = 0; i < sizeof(temp); i++){
-			printf("%c", temp[i]);
+			fprintf(stdout, "%c", temp[i]);
 		}
-		printf("\n");
+		fprintf(stdout, "\n");
 	}
 
-}
-
-void decToBin2(int number){
-	char maradek[20];
-	int i = 0;
-	
-	while (number != 0){
-		if(number % 2 == 0)
-			maradek[i] = '0';
-		if(number % 2 == 1)
-			maradek[i] = '1';
-
-		number /= 2;
-		i++;
-	}
-	maradek[i] = 0;
-	printf("%i\n", i);
-
-	char temp[i + 1];
-	int j = 0;
-	while(i >= 0){
-		temp[j] = maradek[i];
-		i--;
-		j++;
-	}
-
-	for(i = 0; i < sizeof(temp); i++){
-		printf("%c", temp[i]);
-	}
-	printf("\n");
 }
